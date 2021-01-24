f551d769c03c113f0211ef478d4f827e949d54b6
santosh-kumar8367 <61039032+santosh-kumar8367@users.noreply.github.com>
2021-01-24 10:49:43 +0530

    Delete main.py

--- game/main.py
+++
@@ -1,8 +0,0 @@
-from flask import request,Flask,render_template
-app=Flask(__name__)
-@app.route("/")
-def introduce():                                                         
-    return render_template("index.html")
-
-if __name__=='__main__':
-    app.run(threaded=True,port=5000)
